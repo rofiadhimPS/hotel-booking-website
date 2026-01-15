ad14d66b7fc005aa423efc2936db6b6ed6060731
rofiadhim <muhammadrofiadhim@gmail.com>
2026-01-15 13:50:13 +0800

    add login and registration modals to index.php

--- index.php
+++ index.php
@@ -6,12 +6,26 @@
     <title>MR Hotel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
     <style>
         * {
             font-family: 'Poppins', sans-serif;
         }
         .h-font {
             font-family: 'Merienda', cursive;
+        }
+        /* Hide number arrows */
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+          -moz-appearance: textfield;
         }
     </style>
 </head>
@@ -20,7 +34,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 shadow-sm sticky-top">
       <div class="container-fluid">
         <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">MR Hotel</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -41,12 +55,120 @@
             </li>
           </ul>
           <div class="d-flex">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+            <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">
+              Login
+            </button>  
+            <button type="button" class="btn btn-outline-dark shadow-none" data-bs-toggle="modal" data-bs-target="#registerModal">
+              Register
+            </button>  
           </div>
         </div>
       </div>
     </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- Modal -->
+    <div class="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <form action="">
+            <div class="modal-header">
+              <h5 class="modal-title d-flex align-items-center">
+                <i class="bi bi-person-circle fs-3 me-2"></i>User Login
+              </h5>
+              <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="mb-3">
+                <label class="form-label">Email address</label>
+                <input type="email" class="form-control shadow-none">
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control shadow-none">
+              </div>
+              <div class="d-flex align-items-center justify-content-between mb-2">
+                <button type="submit" class="btn btn-dark shadow-none">Login</button>
+                <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="registerModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <form action="">
+            <div class="modal-header">
+              <h5 class="modal-title d-flex align-items-center">
+                <i class="bi bi-person-lines-fill fs-3 me-2"></i></i>User Registration
+              </h5>
+              <button type="reset" class="btn-close shadow-none" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <span class="badge rounded-pill bg-light text-dark mb-3 text-wrap lh-base">
+                Note: Your details must match with your ID (KTP, SIM Card, Passport, etc.) that will be required during check-in. 
+              </span>
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-md-6 ps-0 mb-3">
+                    <label class="form-label">Name</label>
+                    <input type="text" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 p-0 mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 ps-0 mb-3">
+                    <label class="form-label">Phone number</label>
+                    <input type="number" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 p-0 mb-3">
+                    <label class="form-label">Picture</label>
+                    <input type="file" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-12 p-0 mb-3">
+                    <label class="form-label">Address</label>
+                    <textarea class="form-control shadow-none" rows="1"></textarea>
+                  </div>
+                  <div class="col-md-6 ps-0 mb-3">
+                    <label class="form-label">Pincode</label>
+                    <input type="number" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 p-0 mb-3">
+                    <label class="form-label">Date of birth</label>
+                    <input type="date" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 ps-0 mb-3">
+                    <label class="form-label">Password</label>
+                    <input type="password" class="form-control shadow-none">
+                  </div>
+                  <div class="col-md-6 p-0 mb-3">
+                    <label class="form-label">Confirm password</label>
+                    <input type="password" class="form-control shadow-none">
+                  </div>
+                </div>
+              </div>
+              <div class="text-center my-1">
+                <button type="submit" class="btn btn-dark shadow-none">Register</button>
+              </div> 
+              <!-- <div class="mb-3">
+                
+              </div>
+              <div class="mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control shadow-none">
+              </div>
+              <div class="d-flex align-items-center justify-content-between mb-2">
+                <a href="javascript: void(0)" class="text-secondary text-decoration-none">Forgot Password?</a> -->
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>   
 </body>
 </html>
