084f2ad66b9e18114ff4d4b33a1692913cbac111
rofiadhim <muhammadrofiadhim@gmail.com>
2026-01-16 16:41:46 +0800

    add facilities section

--- index.php
+++ index.php
@@ -237,7 +237,7 @@
 
     <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
 
-    <div class="container">
+    <div class="container"> 
       <div class="row">
         <div class="col-lg-4 col-md-6 my-3">
           <div class="card border-0 shadow" style="max-width: 350px; margin: auto;">
@@ -391,10 +391,43 @@
         </div>
 
         <div class="col-lg-12 text-center mt-5">
-          <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms>>></a>
+          <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Rooms >>></a>
         </div>
       </div>
     </div>
+
+    <!-- Our Facilities -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+
+    <div class="container">
+      <div class="row justify-content-evenly px-lg-0 px-5">
+        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+          <img src="images/facilities/wifi.svg" width="80px">
+          <h5 class="mt-3">Wifi</h5>
+        </div>
+        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+          <img src="images/facilities/wifi.svg" width="80px">
+          <h5 class="mt-3">Wifi</h5>
+        </div>
+        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+          <img src="images/facilities/wifi.svg" width="80px">
+          <h5 class="mt-3">Wifi</h5>
+        </div>
+        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+          <img src="images/facilities/wifi.svg" width="80px">
+          <h5 class="mt-3">Wifi</h5>
+        </div>
+        <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+          <img src="images/facilities/wifi.svg" width="80px">
+          <h5 class="mt-3">Wifi</h5>
+        </div>
+        <div class="col-lg-12 text-center mt-5">
+          <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+        </div>
+      </div>
+    </div>
+
 
 
 
