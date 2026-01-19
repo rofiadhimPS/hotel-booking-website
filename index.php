9d028a6f71616f4c0d70146407765209ec4b46ff
rofiadhim <muhammadrofiadhim@gmail.com>
2026-01-19 13:30:38 +0800

    add testimonials section with swiper carousel for user reviews

--- index.php
+++ index.php
@@ -428,8 +428,99 @@
       </div>
     </div>
 
+    <!-- Testimonials -->
 
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
 
+    <div class="container mt-5">
+      <!-- Swiper -->
+      <div class="swiper swiper-testimonials">
+        <div class="swiper-wrapper mb-5">
+          <div class="swiper-slide bg-white p-4">
+            <div class="profile d-flex align-items-center mb-3">
+              <img src="images/about/rating.svg" width="30px">
+              <h6 class="m-0 ms-2">Random user</h6>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Enim eveniet voluptas non temporibus, doloribus voluptatem.
+            </p>
+            <div class="rating">
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+            </div>
+          </div>
+          <div class="swiper-slide bg-white p-4">
+            <div class="profile d-flex align-items-center mb-3">
+              <img src="images/about/rating.svg" width="30px">
+              <h6 class="m-0 ms-2">Random user</h6>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Enim eveniet voluptas non temporibus, doloribus voluptatem.
+            </p>
+            <div class="rating">
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+            </div>
+          </div>
+          <div class="swiper-slide bg-white p-4">
+            <div class="profile d-flex align-items-center mb-3">
+              <img src="images/about/rating.svg" width="30px">
+              <h6 class="m-0 ms-2">Random user</h6>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Enim eveniet voluptas non temporibus, doloribus voluptatem.
+            </p>
+            <div class="rating">
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+            </div>
+          </div>
+          <div class="swiper-slide bg-white p-4">
+            <div class="profile d-flex align-items-center mb-3">
+              <img src="images/about/rating.svg" width="30px">
+              <h6 class="m-0 ms-2">Random user</h6>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Enim eveniet voluptas non temporibus, doloribus voluptatem.
+            </p>
+            <div class="rating">
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+            </div>
+          </div>
+          <div class="swiper-slide bg-white p-4">
+            <div class="profile d-flex align-items-center mb-3">
+              <img src="images/about/rating.svg" width="30px">
+              <h6 class="m-0 ms-2">Random user</h6>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Enim eveniet voluptas non temporibus, doloribus voluptatem.
+            </p>
+            <div class="rating">
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+              <i class="bi bi-star-fill text-warning"></i>
+            </div>
+          </div>
+
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+    </div>
 
 <br><br><br>
 <br><br><br>  
@@ -443,7 +534,7 @@
         spaceBetween: 30,
         effect: "fade",
         autoplay: {
-          delay: 3000,
+            delay: 3000,
           disableOnInteraction: false,
         },
         loop: true,
@@ -452,6 +543,39 @@
       document.querySelector('.swiper-container').addEventListener('click', function() {
         swiper.slideNext();
       });
+
+      var swiper = new Swiper(".swiper-testimonials", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        // slidesPerView: "auto",
+        slidesPerView: "3",
+        loop: true,
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: false,
+        },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
     </script>
 
     
