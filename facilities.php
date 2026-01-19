ddb4946aa936707008a3c2ae171920e2ea340c76
rofiadhim <muhammadrofiadhim@gmail.com>
2026-01-19 16:49:20 +0800

    feat: add facilities page with facility details and styling

---
+++ facilities.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MR Hotel - FACILITIES</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
+    <?php require_once("inc/links.php") ?>
+    <style>
+      .pop:hover {
+        border-top-color: var(--teal) !important;
+        transform: scale(1.03);
+        transition: all 0.3s;
+      }
+    </style>
+</head>
+<body class="bg-light">
+
+    <?php require_once("inc/header.php") ?>
+
+    <div class="my-5 px-4"> 
+      <h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+      <h2 class="h-line bg-dark"></h2>
+      <p class="text-center">
+        Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Aliquid ullam eius<br>
+        dignissimos nesciunt amet explicabo?
+      </p>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5 px-4">
+          <div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+            <div class="d-flex align-items-center mb-2">
+              <img src="images/facilities/wifi.svg" width="40px">
+              <h5 class="m-0 ms-3">Wifi</h5>  
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Ut vitae ullam dolor voluptatibus amet iure fugit cupiditate?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php require_once("inc/footer.php") ?>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MR Hotel</title>
+    <title>MR Hotel - HOME</title>
     <?php require_once("inc/links.php") ?>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.css"/>
     <style>
