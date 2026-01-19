a3ba9c9d18988824ddd6cb13f67cf50587980cb4
rofiadhim <muhammadrofiadhim@gmail.com>
2026-01-19 14:04:08 +0800

    add reach us section with contact information and embedded map

--- index.php
+++ index.php
@@ -522,6 +522,56 @@
       </div>
     </div>
 
+    <!-- Reach Us -->
+
+    <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">REACH US</h2>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded">
+          <iframe class="w-100 rounded" height="400px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18597.305066811517!2d120.440154074333!3d-5.603659754781869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbbf3fa9e2f1c37%3A0x6217cb35c3073b6a!2sPantai%20Tj.%20Bira!5e0!3m2!1sid!2sid!4v1768801103025!5m2!1sid!2sid" loading="lazy"></iframe>
+        </div>
+        <div class="col-lg-4 col-md-4">
+          <div class="bg-white p-4 rounded mb-4">
+            <h5>Call Us</h5>
+            <a href="https://wa.me/6281234567890" class="d-inline-block mb-2 text-decoration-none text-dark">
+              <i class="bi bi-telephone-fill"></i> +628123456789
+            </a>
+            <br>
+            <a href="https://wa.me/6281234567890" class="d-inline-block text-decoration-none text-dark">
+              <i class="bi bi-telephone-fill"></i> +628123456789
+            </a>
+          </div>
+          <div class="bg-white p-4 rounded mb-4">
+            <h5>Follow Us</h5>
+            <a href="#" class="d-inline-block mb-3">
+              <span class="badge bg-light text-dark fs-6 p-2">
+                <i class="bi bi-twitter-x"></i> Twitter
+              </span>
+            </a>
+            <br>
+            <a href="#" class="d-inline-block mb-3">
+              <span class="badge bg-light text-dark fs-6 p-2">
+                <i class="bi bi-instagram"></i> Instagram
+              </span>
+            </a>
+            <br>
+            <a href="#" class="d-inline-block mb-3">
+              <span class="badge bg-light text-dark fs-6 p-2">
+                <i class="bi bi-facebook"></i> Facebook
+              </span>
+            </a>
+            <br>
+            <a href="#" class="d-inline-block mb-3">
+              <span class="badge bg-light text-dark fs-6 p-2">
+                <i class="bi bi-tiktok"></i> Tiktok
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
 <br><br><br>
 <br><br><br>  
     
